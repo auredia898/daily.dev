081adc7cc0ede44d5cad240bd8c2834160856bb8
AWEHA Nicolas <awenico98@gmail.com>
2024-08-08 14:43:38 +0100

    feat: get post by id and posts by users

--- src/features/posts/postRoute.js
+++ src/features/posts/postRoute.js
@@ -3,7 +3,9 @@ const postController = require('./postController')
 const router = express.Router()
 
 router.post('/', postController.createPost)
-router.get('/:squadId', postController.getPostsBySquad)
+router.get('/squad/:squadId', postController.getPostsBySquad)
+router.get('/user/:userId', postController.getPostsByUser)
+router.get('/:postId', postController.getPostById)
 router.put('/:postId', postController.updatePost)
 router.delete('/:postId', postController.deletePost)
 
